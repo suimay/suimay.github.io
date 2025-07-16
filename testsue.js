80dfd794bbf3f6762a54705a08349b46383c2770
suimay <atomsyndrome@gmail.com>
2025-07-16 11:28:32 +0900

    minitest

--- testsue.js
+++ testsue.js
@@ -1,1 +1,77 @@
-//수ㅡ정햇다
+/*
+
+// 변수 미니퀘스트 1
+let name = "웨인";
+let age = 25;
+let height = 177.1;
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+// 변수 미니퀘스트 2
+let name = "웨인";
+let age = 25;
+let height = 177.1;
+
+console.log(name,age, height);
+
+// 변수 미니퀘스트 3
+
+let create = "작성";
+let read = "조회";
+let update = "수정";
+let deleteItem = "삭제";
+let extra = "추가기능";
+let exit = "종료";
+
+console.log(create);
+console.log(read);
+console.log(update);
+console.log(deleteItem);
+console.log(extra);
+console.log(exit);
+
+
+// 조건문 미니퀘스트 1
+//땅버잼 바른 식빵 -> 식빵 준비, 땅콩버터 준비 각 변수 유무 확인 조건문 분기
+let bread = "ready";
+let peanutbutter = "ready";
+let Knife = "ready";
+
+// 식빵 확인
+if (bread !== "ready") {
+    console.log("식빵 없음");
+} 
+else {
+    console.log("식빵 있음");
+    
+    // 땅버 확인
+    if (peanutbutter !== "ready") {
+        console.log("땅버 없음");
+    } 
+    else {
+        console.log("땅버 있음");
+        
+        // 칼 확인
+        if (Knife !== "ready") {
+            console.log("버터나이프 없음");
+        } 
+        else {
+            console.log("버터나이프로 땅버를 식빵에 바름");
+        }
+        
+        
+        // 완료
+        console.log("땅버샌드위치 완성!");
+    }
+}
+*/
+
+
+// 조건문 미니퀘스트 2
+
+
+
+
+// 조건문 미니퀘스트 3
